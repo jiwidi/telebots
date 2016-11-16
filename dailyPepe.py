14b50b4b1962fc3b1b2814a0d3db5eaa6129b232
jaime <fhjaime96@gmail.com>
2016-11-16 18:18:49 +0100

    Add files via upload

--- dailyPepe.py
+++ dailyPepe.py
@@ -8,8 +8,17 @@ pepe="285650927:AAEsuGb5XkzG2WatFjaQdVWLULX3L4lWpYk"
 bot=telebot.TeleBot(pepe)
 @bot.message_handler(commands=['randompepe'])
 def send_Rpepe(message):
-	id=random.randint(0,1000);
-	file= open(id+".jpg",r)
+	type=random.randint(0,2)
+	if(type is 0)
+		id=random.randint(0,1000)
+		type=".jpg"
+	elif(type is 1)
+		id=random.randint(0,1000)
+		type=".png"
+	elif(type is 2)
+		id=random.randint(0,1000)
+		type=".gif"
+	file= open(id+type,r)
 	bot.send_photo(message.chat.id, file);
 	file.close();
 bot.polling()
